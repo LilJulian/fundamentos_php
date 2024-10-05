74d6e76a9af4e4b365619f27439ecc48270b70ed
LilJulian <juliadar267@gmail.com>
2024-10-05 11:00:19 -0500

    fundamentos

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<a href="01-basico/tipo-datos.php">Tipos de datos</a>
+<a href="01-basico/variables.php">Variables</a>
+<a href="01-basico/constantes.php">Contantes</a>
+<a href="01-basico/arrays.php">Arrays</a>
+<a href="01-basico/concatenacion.php">Concatenacion</a>
+<a href="01-basico/operadores_aritmeticos.php">Operadores aritmeticos</a>
+<a href="01-basico/asignacion.php">Operadores de asignacion</a>
+<a href="01-basico/asignacion_por_referencia.php">Operadores de asignacion por referencia</a>
